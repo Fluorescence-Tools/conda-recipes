23884191722facb8274eb50a92c8b7e3a169389a
tpeulen <_Gremlin12>
2018-12-03 20:42:33 -0800

    Created recipe for avtraj & chisurf

--- avtraj/build.sh
+++ avtraj/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # build documentation
-sphinx-apidoc -o doc avtraj
-cd doc
-make html
-cd ..
+#sphinx-apidoc -o doc avtraj
+#cd doc
+#make html
+#cd ..
 
-python setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
 #python setup.py sdist
 #twine upload dist/*
