0a068b8546da991d1cfd51c8c281c15d579e33e0
tpeulen <thomas.otavio.peulen@gmail.com>
2019-09-03 21:35:21 -0700

    Added c library to macos linux installation

--- labellib/build.sh
+++ labellib/build.sh
@@ -1,1 +1,5 @@
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
+mkdir build_lib
+cd build_lib
+cmake ..
+make DESTDIR=$PREFIX install
