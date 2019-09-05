bac3a7e4f2f23c13ef02f144ad71bd6a78da50e3
tpeulen <thomas.otavio.peulen@gmail.com>
2019-09-04 20:18:07 -0700

    Worked on MacOS

--- pymol-oss/build.sh
+++ pymol-oss/build.sh
@@ -1,7 +1,15 @@
 git clone https://github.com/g-truc/glm.git glm_source
 git clone https://github.com/rcsb/mmtf-cpp.git mmtf-cpp
 git clone https://github.com/msgpack/msgpack-c.git msgpack
-mv glm_source/glm ./include
-mv mmtf-cpp/include/* ./include/
-mv msgpack/include/* ./include/
-$PYTHON setup.py install --prefix="$PREFIX" --use-msgpackc c++11 --no-libxml
+mv glm_source/glm $PREFIX/include/
+mv mmtf-cpp/include/* $PREFIX/include/
+mv msgpack/include/* $PREFIX/include/
+
+if [ "$(uname)" == "Darwin" ]; then
+  rm $PREFIX/include/menu.h # necessary because of MacOS file system case insensitivity
+  $PYTHON setup.py install --prefix="$PREFIX" --use-msgpackc=c++11 --no-libxml --no-glut --osx-frameworks
+fi
+if [ "$(uname)" == "Linux" ]; then
+  $PYTHON setup.py install --prefix="$PREFIX" --use-msgpackc=c++11 --no-libxml --no-glut
+fi
+
