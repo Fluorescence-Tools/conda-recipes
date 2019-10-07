e70181e07cbb9d357a2611890878ca9b29a638a2
tpeulen <thomas.otavio.peulen@gmail.com>
2019-10-07 00:35:42 -0700

    Fixed mmCIF recipe
    * Added pycorrfit recepie (works on Ubuntu)
    * Fixed tttrlib

--- pymol-oss/bld.bat
+++ pymol-oss/bld.bat
@@ -1,7 +1,10 @@
-git clone https://github.com/g-truc/glm.git glm_source
 git clone https://github.com/rcsb/mmtf-cpp.git mmtf-cpp
-git clone https://github.com/msgpack/msgpack-c.git msgpack
+move mmtf-cpp/include/* $PREFIX/include/
+
+git clone https://github.com/g-truc/glm.git glm_source
 move glm_source/glm ./include
-move mmtf-cpp/include/* ./include/
+
+git clone https://github.com/msgpack/msgpack-c.git msgpack
 move msgpack/include/* ./include/
-python setup.py install --prefix="$PREFIX" --use-msgpackc c++11 --no-libxml
+
+python setup.py install --prefix="%PREFIX" --use-msgpackc no --no-libxml
